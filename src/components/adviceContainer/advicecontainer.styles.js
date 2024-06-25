57940bd6f085c915587806f72719c5be00fb4ecf
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-06-25 18:51:30 +0200

    media queries

--- src/components/adviceContainer/advicecontainer.styles.js
+++ src/components/adviceContainer/advicecontainer.styles.js
@@ -35,5 +35,27 @@ const StyledContainer = styled.div`
 		left: 50%;
 		transform: translateX(-50%);
 	}
+	@media (max-width: 425px) {
+		width: 343px;
+		height: auto;
+		padding-bottom: 70px;
+		p {
+			margin: 0;
+			color: #53ffaa;
+			text-align: center;
+			font-size: 11px;
+			padding-top: 48px;
+		}
+		h1 {
+			color: #cee3e9;
+			padding: 24px 48px 0px 48px;
+			text-align: center;
+			font-size: 24px;
+		}
+		img {
+			margin: 0 auto;
+			width: 80%;
+		}
+	}
 `;
 export { StyledContainer };
